1f5d47c9a72a28bf4f709e2be6fa0cb6ba558514
devshisir <contact.shisirimran@gmail.com>
2022-08-12 01:22:07 +0600

    Add scss source-map options

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
 
 // compile scss into css
 function style(){
     return gulp.src('asset/sass/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('asset/css'))
     .pipe(browserSync.stream());
 }
@@ -18,6 +21,7 @@ function watch(){
     });
     gulp.watch('./asset/sass/*.scss', style);
     gulp.watch('./asset/sass/component/*.scss', style);
+    gulp.watch('./asset/sass/import/*.scss', style);
     gulp.watch('./asset/sass/page/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./asset/css/*.css').on('change', browserSync.reload);
